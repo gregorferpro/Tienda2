ef8598852b21532be0f52b5af4c06cd6522150b7
gregorferpro <gregorbarrios07@gmail.com>
2026-04-16 06:20:37 -0400

    Corrige carga de imagenes media en produccion

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from cuentas import views as cuentas_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('crear-admin-render/', cuentas_views.crear_admin_render, name='crear_admin_render'),
     path('configurar-google-render/', cuentas_views.configurar_google_render, name='configurar_google_render'),
 
@@ -16,12 +17,11 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', email_template_name='registration/password_reset_email.html', subject_template_name='registration/password_reset_subject.txt'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
     path('', include('cuentas.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir media también en local y en Render
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
